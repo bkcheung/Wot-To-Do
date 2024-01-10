d968ebe95bf3670cad6a54782ff19cebd3fe5d4d
bkcheung <b.cheung@hotmail.com>
2024-01-10 17:44:00 -0500

    Set up page rendering with js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,42 @@ import _ from 'lodash';
 import './style.css';
 import addTask from './tasks';
 
-function genHeader() {
-    const head = document.createElement('div');  
-    head.innerHTML = "Wot To Do";
-    head.classList.add('header');
-    return head;
+function render() {
+    const page = document.createElement('div');
+    page.classList.add('page');
+
+    const header = document.createElement('div');  
+    header.innerHTML = "✓✓ Wot To Do";
+    header.classList.add('header');
+    page.appendChild(header);
+
+    const pageBody = document.createElement('div');
+    pageBody.classList.add('pageBody');
+    const projList = document.createElement('div');
+    projList.innerHTML = "Today";
+    projList.classList.add('projList');
+    pageBody.appendChild(projList);
+
+    const taskList = document.createElement('div');
+    taskList.innerHTML = "Exercise";
+    taskList.classList.add('taskList');
+    pageBody.appendChild(taskList);
+    page.appendChild(pageBody);
+
+    const footer = document.createElement('footer');
+    footer.classList.add('footer');
+    const year = new Date().getFullYear();
+
+    const github = document.createElement('a');
+    github.classList.add('github');
+    github.href = "https://github.com/bkcheung",
+    github.innerHTML = "bkcheung";
+    footer.innerHTML = `Copyright © ${year} `;
+    footer.appendChild(github);
+
+    page.appendChild(footer);
+
+    return page;
   }
   
-  document.body.appendChild(genHeader());
+  document.body.appendChild(render());
